1f99fa71883080edb1e60dac7320ab310a2968f4
Vilen Osipov <osipovvilen03@gmail.com>
2025-09-22 10:59:08 +0300

    T5 init

---
+++ tasks/task5/booking-service/v1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	enableFeatureX := os.Getenv("ENABLE_FEATURE_X") == "true"
+
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "pong")
+	})
+
+	if enableFeatureX {
+		http.HandleFunc("/feature", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Feature X is enabled!")
+		})
+	}
+
+	log.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ tasks/task5/booking-service/v2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "pong")
+	})
+
+	http.HandleFunc("/feature", func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("x-feature-enabled") == "true" {
+			fmt.Fprintf(w, "Feature X is enabled!")
+		} else {
+			fmt.Fprintf(w, "Got your request, but feature X is disabled.")
+		}
+	})
+
+	log.Println("Server runnning on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
